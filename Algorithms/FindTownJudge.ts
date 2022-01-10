dff70b6c25a6eaed6ad3c2212ee067acba7814d0
Nick Gallo <nick.gallo@variantyx.com>
2022-01-10 09:41:10 -0500

    fix find the town judge

--- Algorithms/FindTownJudge.ts
+++ Algorithms/FindTownJudge.ts
@@ -1,18 +1,16 @@
 export class FindTownJudge {
   run(numPeople: number, voteGraph: number[][]): number {
-    if(numPeople === 1) return numPeople
+    if (numPeople === 1) return numPeople
     
-    let voteTallies = new Array(numPeople + 1).fill(0)
+    const voteTallies = new Array(numPeople + 1).fill(0)
+
     for (const [personA, personB] of voteGraph) {
       --voteTallies[personA]
       ++voteTallies[personB]
     }
-
-    /*
+    
     for (const [person, votes] of voteTallies.splice(0).entries())
       if (votes === numPeople - 1) return person
-    */
-    const judge: number = voteTallies.splice(0).find( (vote: number) => vote === numPeople - 1)[0]
-    return judge ? judge : -1
+    return -1
   }
 }
